f963d183c7e600ff1ec2847746a1053bfb954e05
ARE PARAMESWARUDU <127785890+AreParameswarudu@users.noreply.github.com>
2025-10-09 11:40:27 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
     region = "ap-south-1"
 }
 
-resources "aws_instance" "my-instances" {
+resource "aws_instance" "my-instances" {
 
     instance_type = "t2.micro"
     ami = "ami-283ync349ry"
